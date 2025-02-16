f678e6ea8d057fad1d76cbced04e8444ab8b553e
AmirSalahY <amir@noury.ch>
2025-02-16 12:32:03 +0200

    Add CORS support and fetch transactions in frontend

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
-from flask import Flask #, request
+from flask import Flask, request
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/")
 def transactions_list():
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
 function App() {
+  // use data to display the fetched transactions
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch("http://127.0.0.1:5000/");
+    const fetchedData = await response.json();
+    setData(fetchedData);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to UR-Store Assessment</h1>
+      {/* implement the ui here */}
     </div>
   );
 }
